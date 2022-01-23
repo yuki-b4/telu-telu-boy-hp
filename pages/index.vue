5c5b3f8d53ca58eeaeb97fa43791bef4ad27b241
yuki-b4 <capou0872@gmail.com>
2022-01-23 17:41:50 +0900

    contentfulで設定、indexページへの出力

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,34 @@
 <template>
-  <Tutorial />
+  <section class="index">
+    <card
+      v-for="(post,i ) in posts"
+      :key="i"
+      :title="post.fields.title"
+      :id="post.sys.id"
+      :date="post.sys.updatedAt"
+    />
+  </section>
 </template>
 
 <script>
+import Card from '~/components/card.vue'
+import { createClient } from '~/plugins/contentful.js'
+
+const client = createClient()
 export default {
-  name: 'IndexPage',
+  transition: 'slide-left',
+  components: {
+    Card
+  },
+  asyncData({ env, params }) {
+    return client
+      .getEntries(env.CTF_BLOG_POST_TYPE_ID)
+      .then(entries => {
+        return {
+          posts: entries.items
+        }
+      })
+      .catch(console.error)
+  }
 }
 </script>
